bcf653a2ba796e0bc57db026bb9e3b07ba9606f8
chengmengyi@mail.dataoke.com <chengmengyi@mail.dataoke.com>
2020-09-24 13:44:46 +0800

    修改demoid

--- app/src/main/java/com/dataoke/bxkadsdk/ApiParams.java
+++ app/src/main/java/com/dataoke/bxkadsdk/ApiParams.java
@@ -2,17 +2,17 @@ package com.dataoke.bxkadsdk;
 
 public class ApiParams {
     //开屏广告type id
-    public static final String OPEN_SCREEN_COMID="1886";
+    public static final String OPEN_SCREEN_COMID="2623";
 
     //信息流组件
-    public static final String INFO_FLOW_COMID="1889";
+    public static final String INFO_FLOW_COMID="2621";
 
     //搜索组件
-    public static final String SEARCH_COMID="1883";
+    public static final String SEARCH_COMID="2622";
 
     //弹窗组件
-    public static final String DIALOG_COMID="1885";
+    public static final String DIALOG_COMID="2620";
 
     //浮窗组件
-    public static final String FLOAT_COMID="1885";
+    public static final String FLOAT_COMID="2624";
 }
